df799023df98ec5cc390f1080c5559fd20f92915
Waqas, Rana Muhammad <rana.waqas@afiniti.com>
2022-02-05 02:39:21 +0500

    updated the maven file for build and some minor changes.

--- src/main/java/com/commandcenter/datacollector/App.java
+++ src/main/java/com/commandcenter/datacollector/App.java
@@ -1,26 +1,26 @@
 package com.commandcenter.datacollector;
 
-import com.commandcenter.datacollector.shutdown.ShutdownThread;
-import com.commandcenter.datacollector.logger.Logger;
+import jdk.jpackage.internal.Log;
 
 public class App {
 
     public static void main(String[] args) {
-        Logger.LogInfo(new Object() {
-        }.getClass().getEnclosingClass().getSimpleName(), "------- Starting the FTP Download Thread ------");
-//        AutoCallHandler autoCallHandler = new AutoCallHandler();
+//        Logger.LogInfo(new Object() {
+//        }.getClass().getEnclosingClass().getSimpleName(), "------- Starting the FTP Download Thread ------");
+////        AutoCallHandler autoCallHandler = new AutoCallHandler();
+//
+////        Thread autoCallHandlerThread = new Thread(autoCallHandler);
+//        Thread autoCallHandlerThread = new Thread();
+//        autoCallHandlerThread.setName("Auto Call Handler Thread");
+//        autoCallHandlerThread.start();
+//        Logger.LogInfo(new Object() {
+//        }.getClass().getEnclosingClass().getSimpleName(), "---------Thread has been started ---------");
+//
+//        Logger.LogInfo(new Object() {
+//        }.getClass().getEnclosingClass().getSimpleName(), "Adding shutdown hook for the auto call notification service.");
+//        Runtime r = Runtime.getRuntime();
+//        r.addShutdownHook(new ShutdownThread());
 
-//        Thread autoCallHandlerThread = new Thread(autoCallHandler);
-        Thread autoCallHandlerThread = new Thread();
-        autoCallHandlerThread.setName("Auto Call Handler Thread");
-        autoCallHandlerThread.start();
-        Logger.LogInfo(new Object() {
-        }.getClass().getEnclosingClass().getSimpleName(), "---------Thread has been started ---------");
-
-        Logger.LogInfo(new Object() {
-        }.getClass().getEnclosingClass().getSimpleName(), "Adding shutdown hook for the auto call notification service.");
-        Runtime r = Runtime.getRuntime();
-        r.addShutdownHook(new ShutdownThread());
-
+        Log.info("Starting the application");
     }
 }
