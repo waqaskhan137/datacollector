6151235d146264c8d9fee5251a049c24229e106a
Waqas, Rana Muhammad <rana.waqas@afiniti.com>
2022-02-06 06:34:22 +0500

    Added IMAP and POP implementation for email MSExchange restructure

--- src/main/java/com/commandcenter/datacollector/plugins/inputs/snmpagent/snmp.java
+++ src/main/java/com/commandcenter/datacollector/plugins/inputs/snmpagent/snmp.java
@@ -3,4 +3,18 @@ package com.commandcenter.datacollector.plugins.inputs.snmpagent;
 import com.commandcenter.datacollector.plugins.inputs.Input;
 
 public class snmp implements Input {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void fetch() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
 }
