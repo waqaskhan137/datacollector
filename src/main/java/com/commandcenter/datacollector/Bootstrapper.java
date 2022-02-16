e56948cd9d98838be8de51691cc8a3ae5e660c4b
Waqas, Rana Muhammad <rana.waqas@afiniti.com>
2022-02-16 06:54:52 +0500

    added Bootstrapper.java for commandline support for the application.

---
+++ src/main/java/com/commandcenter/datacollector/Bootstrapper.java
@@ -0,0 +1,37 @@
+package com.commandcenter.datacollector;
+
+import com.commandcenter.datacollector.plugins.inputs.email.MSExchange;
+import com.commandcenter.datacollector.plugins.inputs.email.message.Message;
+import com.commandcenter.datacollector.plugins.inputs.email.message.MessageList;
+import com.commandcenter.datacollector.plugins.outputs.postgres.Postgres;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Bootstrapper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Bootstrapper.class);
+    private AnnotationConfigApplicationContext context;
+
+    public void start() {
+        LOGGER.info("Starting the application");
+        context = new AnnotationConfigApplicationContext();
+        context.scan("com.commandcenter.datacollector");
+        context.refresh();
+
+        var msExchange = context.getBean(MSExchange.class);
+        var postgres = context.getBean(Postgres.class);
+
+        msExchange.start();
+        MessageList messageList = msExchange.fetch();
+
+        postgres.connect();
+        for (Message message : messageList.getMessages()) {
+            postgres.insert(message);
+        }
+        postgres.disconnect();
+        msExchange.stop();
+
+    }
+
+
+}
--- src/main/java/com/commandcenter/datacollector/plugins/inputs/Input.java
+++ src/main/java/com/commandcenter/datacollector/plugins/inputs/Input.java
@@ -1,9 +1,24 @@
 package com.commandcenter.datacollector.plugins.inputs;
 
 import com.commandcenter.datacollector.plugins.inputs.email.message.MessageList;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface Input {
+    /**
+     * Start the input.
+     */
     public void start();
+
+    /**
+     * Get the messages from the input.
+     *
+     * @return
+     */
     public MessageList fetch();
+
+    /**
+     * Stop the input.
+     */
     public void stop();
 }
